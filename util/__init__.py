7477c4b1a3b9f38b5469a2cf7b196ba7505fb4ca
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 03:01:58 +0000

    Adding tarfile member sanitization to extractall()

--- util/__init__.py
+++ util/__init__.py
@@ -77,12 +77,50 @@ def get_files(path, value):
 
 def extract_bz2(filename, path="."):
     with tarfile.open(filename, "r:bz2") as tar:
-        tar.extractall(path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path)
 
 
 def extract_tar(filename, path="."):
     with tarfile.open(filename) as tar:
-        tar.extractall(path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path)
 
 
 def extract_zip(filename, path="."):
